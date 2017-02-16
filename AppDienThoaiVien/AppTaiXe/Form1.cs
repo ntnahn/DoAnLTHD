21ba206c36fd66a07949d5d26e12118c70c55c71
THANHNHAN <ntnahn@gmail.com>
2017-02-16 17:20:12 +0700

    Add app Tài xế

---
+++ AppDienThoaiVien/AppTaiXe/Form1.cs
@@ -0,0 +1,21 @@
+﻿using Quobject.SocketIoClientDotNet.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AppTaiXe {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            var socket = IO.Socket("http://localhost:8080/");
+            socket.On(Socket.EVENT_CONNECT, () => {
+                //socket.Emit("hi");
+            });
+        }
+    }
+}
